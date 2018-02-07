e8cbadf6441132a0dde94b70f968348590303632
ArturVilkicki <30698622+ArturVilkicki@users.noreply.github.com>
2018-02-07 20:51:59 +0200

    TopoCentras

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TopoCentra parduotuve!!!</title>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Encode+Sans+Expanded" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<script src="JavaScript.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col" style="margin-left: 5px;margin-right: 5px; border-bottom: 1px solid gray">
+				<ul class="nav">
+  					<li class="nav-item">
+  						<a class="nav-link active" href="https://www.topocentras.lt/parduotuviu-tinklas/">Parduotuves<i class="fas fa-angle-right"></i></a>
+  					</li>
+  					<li class="nav-item" style="padding-right: 2cm;">
+  						<a class="nav-link" href="https://www.topocentras.lt/naujoji-topocentro-aplikacija">Aplikacija<i class="fas fa-mobile-alt"></i></a>
+  					</li>
+  					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/nemokamas-atsiemimas">Nemokamas atsiemimas<i class="fas fa-home"></i></a>
+  					</li>
+  					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/pristatymo-salygos">Pristatymas per 1 - 3 d.d.<i class="fas fa-truck"></i></a>
+ 					</li>
+  					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/mokejimo-budai">Atsiskaitymas grynais<i class="far fa-credit-card"></i></a>  
+  					</li>
+  					<li class="nav-item" style="padding-right: 1cm;">
+  						<a class="nav-link" href="https://www.topocentras.lt/14-dienu-grazinimo-garantija">14 d. grazinimo garantija</a>
+ 					</li>
+  					<li class="nav-item">
+  						<button type="button" class="btn btn-primary">Prisijungti<a class="nav-link" href="#"></a></button>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-3">
+				<img src="Topo_centras_logo_naujas.jpg" style="width: 300px;height: 150px;">
+			</div>
+			<div class="col-3">
+				<form style="padding-top: 70px;">
+					<input type="text" name="paieska" size="20" placeholder="Iveskite ieskoma preke">
+					<i class="fas fa-search"></i><input type="submit" name="Rasti" value="Rasti" style="background-color: #7094db;">
+				</form>
+			</div>
+			<div class="col-2">
+				<i class="fas fa-phone" style="float: left; padding-top: 35px;"></i><p style="padding-top: 35px;">8 800 60200</p>
+				<p style="font-size: 12px; margin-left: 19px;">I-V 08:00-20:00 </p>
+			</div>
+			<div class="col-2" style="margin-top: 20px;">
+				<div style="margin-top: 10px; padding-top: 10px; border: 1px solid black; width: 200px; height: 40px;">
+					<i class="fas fa-shopping-cart" style="margin-left: 0px; float: left;"></i><p style="text-align: center;">Krepselis tuscias!!!</p>
+				</div>
+				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<ul class="nav" style="background-color: blue; float: left;">
+  					<li class="nav-item">
+  						<a class="nav-link active" href="https://www.topocentras.lt/parduotuviu-tinklas/" style="color: white;">Akcijos ir pasiulymai</a>
+  					</li>
+  					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/naujoji-topocentro-aplikacija" style="color: white;">Paslaugos</a>
+  					</li>
+  					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/nemokamas-atsiemimas" style="color: white;">Kompiuterine technika</a>
+  					</li>
+  					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/pristatymo-salygos" style="color: white;">Telefonai, video, foto, auto</a>
+ 					</li>
+  					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/mokejimo-budai" style="color: white;">Televizoriai ir garso technika</a>  
+  					</li>
+  					<li class="nav-item" style="padding-right: 1cm;">
+  						<a class="nav-link" href="https://www.topocentras.lt/14-dienu-grazinimo-garantija" style="color: white;">Sveikata, grozis, laisvalaikis</a>
+ 					</li>
+  					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/14-dienu-grazinimo-garantija" style="color: white;">Buitine technika</a>
+					</li>
+					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/14-dienu-grazinimo-garantija" style="color: white;">Miele parduotuve</a>
+					</li>
+					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/14-dienu-grazinimo-garantija" style="color: white;">Apple parduotuve</a>
+					</li>
+					<li class="nav-item">
+  						<a class="nav-link" href="https://www.topocentras.lt/14-dienu-grazinimo-garantija" style="color: white;">Isparduotuve</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="margin-left: 25px;">
+  					<ol class="carousel-indicators">
+    					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">asa</li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="1">asa</li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="2">assaa</li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="3">sasaa</li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="4">asssasa</li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="5">saaaasaa</li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="6">assa</li>
+    					<li data-target="#carouselExampleIndicators" data-slide-to="7">assaa</li>
+  					</ol>
+  					<div class="carousel-inner" role="listbox">
+    					<div class="carousel-item active">
+      						<img class="d-block img-fluid" src="pirmaNuotrauka.jpg" alt="First slide">
+    					</div>
+    					<div class="carousel-item">
+      						<img class="d-block img-fluid" src="antraNuotrauka.jpg" alt="Second slide">
+    					</div>
+    					<div class="carousel-item">
+      						<img class="d-block img-fluid" src="treciaNuotrauka.jpg" alt="Third slide">
+    					</div>
+    					<div class="carousel-item">
+      						<img class="d-block img-fluid" src="ketvirtaNuotrauka.jpg" alt="Third slide">
+    					</div>
+    					<div class="carousel-item">
+      						<img class="d-block img-fluid" src="penktaNuotrauka.jpg" alt="Third slide">
+    					</div>
+    					<div class="carousel-item">
+      						<img class="d-block img-fluid" src="sestaNuotrauka.jpg" alt="Third slide">
+    					</div>
+    					<div class="carousel-item">
+      						<img class="d-block img-fluid" src="septintaNuotrauka.jpg" alt="Third slide">
+    					</div>
+    					<div class="carousel-item">
+      						<img class="d-block img-fluid" src="astuntaNuotrauka.jpg" alt="Third slide">
+    					</div>
+  					</div>
+  					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    					<span class="sr-only">Previous</span>
+  					</a>
+  					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    					<span class="sr-only">Next</span>
+  					</a>
+
+				</div>
+				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12" >
+				<div style="float: left;">
+					<button data-target="#carouselExampleIndicators" data-slide-to="0" style="font-size: 10px; padding: 3px;">TOP kainos LG televizoriams</button>
+				</div>
+				<div style="float: left;">
+					<button data-target="#carouselExampleIndicators" data-slide-to="1" style="font-size: 10px; padding: 3px;">SAMSUNG technikos ispardavimas</button>
+				</div>
+				<div style="float: left;">
+					<button data-target="#carouselExampleIndicators" data-slide-to="2" style="font-size: 10px; padding: 3px;">NAUJIENA | Zaidimai ir ju iranga</button>
+				</div>
+				<div style="float: left;">
+					<button data-target="#carouselExampleIndicators" data-slide-to="3" style="font-size: 10px; padding: 3px;">Lenovo „Jedi Challenges AR” rinkinys</button>
+				</div>
+				<div style="float: left;">
+					<button data-target="#carouselExampleIndicators" data-slide-to="4" style="font-size: 10px; padding: 3px;">Naujasis SAMSUNG Galaxy A8</button>
+				</div>
+				<div style="float: left;">
+					<button data-target="#carouselExampleIndicators" data-slide-to="5" style="font-size: 10px; padding: 3px;">DELONGHI DOLCE GUSTO MINI ME -40 %</button>
+				</div>
+				<div style="float: left;">
+					<button data-target="#carouselExampleIndicators" data-slide-to="6" style="font-size: 10px; padding: 3px;">„iPhone 6“ tik 379 €</button>
+				</div>
+				<div style="float: left;">
+					<button data-target="#carouselExampleIndicators" data-slide-to="7" style="font-size: 10px; padding: 3px;">Įsigyk MacBook Air ir gauk ausines Beats EP</button>
+				</div>
+				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<div style="float: left; position: relative;">
+					<img src="Snai_per_kainas_940x220.jpg" hspace="5"  style="display: inline-block; border-radius: 2px; border: 10px groove white;width: 500px; height: 200px;">
+				</div>
+				<div style="float: left; position: relative;">
+					<img src="download.jpg" hspace="5" style="border:10px groove white ">
+				</div>
+				<div style="float: left; position: relative;">
+					<img src="Omniva_svetaines-baneriai-korekcija-1270x210_11.jpg" style="display: inline-block; border-radius: 2px; border: 10px groove white;width: 500px; height: 200px;">
+				</div>
+			</div>
+		</div>
+	</div>	
+</body>
+</html>
